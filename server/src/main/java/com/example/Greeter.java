25b501547eb925474ae90ff7943dc92ad80832d6
SavadgeCoder <hsl99299@uga.edu>
2024-04-09 14:02:25 -0400

    Hunter Lovejoy wuz here.

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -4,7 +4,8 @@ package com.example;
  * This is a class.
  */
 public class Greeter {
-
+	System.out.println("Hi!");
+	System.out.println("This is a test run with GitHub and it sucks.");
   /**
    * This is a constructor.
    */
@@ -15,5 +16,6 @@ public class Greeter {
   //TODO: Add javadoc comment
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
+    
   }
 }
